bc382994aac6cf0e2dabe14493783bded166d318
akash-0233 <akashrajput0233@gmail.com>
2024-08-10 15:03:06 +0530

    Clone Pancake Price fetch app

---
+++ AbiInfo.js
@@ -0,0 +1,18 @@
+const erc20ABI = ["function decimals() public view returns (uint8)"];
+
+//It will return the address fo liquidity Provider contract
+const factoryABI = [
+  "    function getPair(address tokenA, address tokenB) external view returns (address pair)",
+];
+
+const pairABI = [
+  "function token0() external view returns (address)",
+  "function token1() external view returns (address)",
+  "function getReserves() external view returns (uint112 reserver0, uint112 reserve1, uint32 blockTimestampLast)",
+];
+
+const routerABI = [
+  "function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)",
+];
+
+module.exports = { erc20ABI, factoryABI, pairABI, routerABI };
---
+++ README.md
@@ -0,0 +1,3 @@
+# FlashLoan
+npm init --y
+npm install ethers@5.0.0
